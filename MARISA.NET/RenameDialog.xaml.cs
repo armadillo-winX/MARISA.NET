5b42e1fe3296ff33216584b24eb0ba2abced0a4e
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-09-03 18:41:22 +0900

    RenameDialog実装

--- MARISA.NET/RenameDialog.xaml.cs
+++ MARISA.NET/RenameDialog.xaml.cs
@@ -1,27 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace MARISA.NET
+﻿namespace MARISA.NET
 {
     /// <summary>
     /// RenameDialog.xaml の相互作用ロジック
     /// </summary>
     public partial class RenameDialog : Window
     {
+        private string? _replayFileName;
+
+        public string? ReplayFileName
+        {
+            get
+            {
+                return _replayFileName;
+            }
+
+            set
+            {
+                _replayFileName = value;
+                FileNameBox.Text = value;
+            }
+        }
+
         public RenameDialog()
         {
             InitializeComponent();
+        }
+
+        private void RenameButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (FileNameBox.Text.Length > 0)
+            {
+                this.ReplayFileName = FileNameBox.Text;
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show(this, "ファイル名を空にはできません。", "リプレイファイルのリネーム",
+                    MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
         }
     }
 }
