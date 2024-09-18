d617d41f986895baafe16c995fa09100ea479953
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-09-19 08:14:59 +0900

    GameIndex 実装刷新

--- MARISA.NET/GameIndex.cs
+++ MARISA.NET/GameIndex.cs
@@ -1,33 +1,43 @@
-﻿namespace MARISA.NET
+﻿using System.Collections.Generic;
+
+namespace MARISA.NET
 {
     internal class GameIndex
     {
-        public static string Th06 => "Th06";
+        private readonly static Dictionary<string, string> _gameNameDictionary = new()
+        {
+            { "Th06", "東方紅魔郷" },
+            { "Th07", "東方妖々夢" },
+            { "Th08", "東方永夜抄" },
+            { "Th09", "東方花映塚" },
+            { "Th095", "東方文花帖" },
+            { "Th10", "東方風神録" },
+            { "Th11", "東方地霊殿" },
+            { "Th12", "東方星蓮船" },
+            { "Th125", "ダブルスポイラー" },
+            { "Th128", "妖精大戦争" },
+            { "Th13", "東方神霊廟" },
+            { "Th14", "東方輝針城" },
+            { "Th143", "弾幕アマノジャク" },
+            { "Th15", "東方紺珠伝" },
+            { "Th16", "東方天空璋" },
+            { "Th165", "秘封ナイトメアダイアリー" },
+            { "Th17", "東方鬼形獣" },
+            { "Th18", "東方虹龍洞" },
+            { "Th185", "バレットフィリア達の闇市場" },
+            { "Th19", "東方獣王園" }
+        };
 
-        public static string Th07 => "Th07";
-
-        public static string Th08 => "Th08";
-
-        public static string Th09 => "Th09";
-
-        public static string Th10 => "Th10";
-
-        public static string Th11 => "Th11";
-
-        public static string Th12 => "Th12";
-
-        public static string Th13 => "Th13";
-
-        public static string Th14 => "Th14";
-
-        public static string Th15 => "Th15";
-
-        public static string Th16 => "Th16";
-
-        public static string Th17 => "Th17";
-
-        public static string Th18 => "Th18";
-
-        public static string Th19 => "Th19";
+        public static string GetGameName(string gameId)
+        {
+            if (_gameNameDictionary.TryGetValue(gameId, out string? gameName))
+            {
+                return gameName;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
     }
 }
