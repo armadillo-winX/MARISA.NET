7ed57221795a2881285538adf63a007abc56f9eb
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-09-19 19:19:06 +0900

    ReplayFileInfo実装

---
+++ MARISA.NET/ReplayBackupInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MARISA.NET
+{
+    internal class ReplayBackupInfo
+    {
+        public string? GameId { get; set; } 
+
+        public string? SourceFilePath { get; set; }
+
+        public string? Comment { get; set; }
+    }
+}
