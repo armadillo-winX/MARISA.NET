152d31e399eae8fe5046c3580817ac049ec88aae
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-11-04 00:40:32 +0900

    Revert "OpenBackupDirectoryButton実装"
    This reverts commit 362f0a23d1432490da6757f3c242b0959c0e9c52.

--- MARISA.NET/MainWindow.xaml.cs
+++ MARISA.NET/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ global using System.Windows;
 using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
@@ -384,19 +383,6 @@ namespace MARISA.NET
                     CommentBox.Text = $"エラー\nバックアップに付けられたコメントを読み込めませんでした。\n{ex.Message}";
                 }
             } 
-        }
-
-        private void OpenBackupDirectoryButtonClick(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Process.Start("explorer.exe", PathInfo.ReplayFileBackupDirectory);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(this, ex.Message, "エラー",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
         }
     }
 }
